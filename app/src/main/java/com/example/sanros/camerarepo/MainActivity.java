ea8bfd26f84b9849fb4f64d44f227d6d91149e23
rossolutions <juancarlosrosito@gmail.com>
2017-08-04 14:10:14 -0600

    The Camera is set up

--- app/src/main/java/com/example/sanros/camerarepo/MainActivity.java
+++ app/src/main/java/com/example/sanros/camerarepo/MainActivity.java
@@ -1,13 +1,59 @@
 package com.example.sanros.camerarepo;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.drawable.Drawable;
+import android.net.Uri;
+import android.provider.MediaStore;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
-public class MainActivity extends AppCompatActivity {
+import java.io.File;
+
+public class MainActivity extends Activity {
+    private Button button;
+    private ImageView imageView;
+    private static final int CAM_REQUEST = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        button = (Button) findViewById(R.id.button_id);
+        imageView = findViewById(R.id.image_view);
+        button.setOnClickListener( new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                Intent camera_intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                File file = getFile();
+                camera_intent.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(file));
+                startActivityForResult(camera_intent, CAM_REQUEST);
+            }
+        });
+    }
+
+    private File getFile(){
+
+        File folder = new File("sdcard/camera_app");
+        if(!folder.exists()){
+            folder.mkdir();
+        }
+
+        File image_file = new File(folder,"TIGO_CUADRO1.jpg");
+
+
+        return image_file;
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        String path = "sdcard/camera_app/TIGO_CUADRO1.jpg";
+        imageView.setImageDrawable(Drawable.createFromPath(path));
+
     }
 }
